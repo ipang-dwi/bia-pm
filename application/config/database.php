e6ab074f2979cff23ae1d230279a2a6baa855eda
ipang dwi <admin@firstplato.com>
2018-09-13 14:42:39 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '1717';
+$db['default']['password'] = '';
 $db['default']['database'] = 'bia-pm';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
